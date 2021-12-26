433f39950127ee8e94cd6b1ba84b5c0ace621eae
aymanyou15 <aymanyou15@gmail.com>
2021-12-26 22:45:57 +0200

    fix the buttom issue, set some colors to spedometer

--- JavaMotorDriver/src/javamotordriver/FXMLDocumentController.java
+++ JavaMotorDriver/src/javamotordriver/FXMLDocumentController.java
@@ -8,12 +8,13 @@ import eu.hansolo.medusa.Gauge;
 import java.awt.Button;
 import java.net.URL;
 import java.util.ResourceBundle;
-import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Slider;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
 
 /**
  *
@@ -21,32 +22,60 @@ import javafx.scene.layout.AnchorPane;
  */
 public class FXMLDocumentController implements Initializable {
 
-    public AnchorPane anchor;
+    AnchorPane anchor;
 
-    public Button speed;
-    
+    Button btn;
+
     public Slider verticalSlider;
-    
+
     public Gauge spedometer;
-    
-    int x=0;
-    
+
+    int x = 0;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
-
-    public void anchorOnKeyPressed(KeyEvent keyEvent){
-        
+        spedometer.setNeedleColor(Color.RED);
     }
-    
 
-    public void btnOnMouseClick(MouseEvent mouseEvent){
-        System.out.printf("ahhhhhhhhhhh");
+    public void btnMouseClicked(MouseEvent mouseEvent) {
+        x += 5;
+        if (spedometer.getValue() >= 50) {
+            x = 50;
+        }
+        if (spedometer.getValue() < 0) {
+            x = 0;
+        }
+        if (spedometer.getValue() >= 30) {
+            spedometer.setTickMarkColor(Color.RED);
+            spedometer.setTickLabelColor(Color.WHITE);
+            spedometer.setBarColor(Color.RED);
+            spedometer.setValueColor(Color.RED);
+            spedometer.setUnitColor(Color.RED);
+            spedometer.setTitleColor(Color.RED);
+        }
+        if (spedometer.getValue() < 30) {
+            spedometer.setTickMarkColor(Color.BLUE);
+            spedometer.setTickLabelColor(Color.WHITE);
+            spedometer.setBarColor(Color.BLUE);
+            spedometer.setValueColor(Color.WHITE);
+            spedometer.setUnitColor(Color.WHITE);
+            spedometer.setTitleColor(Color.WHITE);
+        }
         spedometer.setValue(x);
-        x+=5;
     }
-    
-    
+
+    public void onKeyPressed(KeyEvent keyEvent) {
+        // soliman's code
+        
+        /* dummy code to check the key event */
+        if (keyEvent.getCode() == KeyCode.UP){
+            verticalSlider.setValue(10);
+        }
+        else if (keyEvent.getCode() == KeyCode.END){            
+            spedometer.setValue(0);
+            x = 0;
+        }
+    }
+
 }
