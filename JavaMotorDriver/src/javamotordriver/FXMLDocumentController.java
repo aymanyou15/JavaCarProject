80f7d8a2e3eec99576053eda015cbb673bb348d5
DESKTOP-4J1JK3U\DELL <marwa-elnobi98@outlook.com>
2021-12-27 18:39:59 +0200

    Add S & B key event to control the speed through the Slider

--- JavaMotorDriver/src/javamotordriver/FXMLDocumentController.java
+++ JavaMotorDriver/src/javamotordriver/FXMLDocumentController.java
@@ -20,6 +20,7 @@ import javafx.scene.control.ComboBox;
 import javafx.scene.control.Slider;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseDragEvent;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.paint.Color;
@@ -35,6 +36,7 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     Button btn;
     KeyCode key;
+
     OutputStream out;
     Scanner in;
     @FXML
@@ -51,87 +53,122 @@ public class FXMLDocumentController implements Initializable {
         // TODO
         spedometer.setNeedleColor(Color.RED);
         btn.setText("Start");
-        
+
         // populate the drop-down box
-	SerialPort[] portNames = SerialPort.getCommPorts();
+        SerialPort[] portNames = SerialPort.getCommPorts();
         for (SerialPort portName : portNames) {
             portList.getItems().addAll(portName.getSystemPortName());
         }
     }
 
     public void btnMouseClicked(MouseEvent mouseEvent) {
-         if(btn.getText().equals("Start")) {
-                // attempt to connect to the serial port
-                chosenPort = SerialPort.getCommPort(portList.getValue().toString());
-                chosenPort.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0);
-                if(chosenPort.openPort()) {
-                        btn.setText("End");
-                        portList.setEditable(false);
-                         out = chosenPort.getOutputStream();
-                }
-                
-                    
-                } else {
-                        // disconnect from the serial port
-                        chosenPort.closePort();
-                        try {
-                            out.close();
-                        } catch (IOException ex) {
-                            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
-                        }
-                        portList.setEditable(true);
-                        btn.setText("Start");
+        if (btn.getText().equals("Start")) {
+            // attempt to connect to the serial port
+            chosenPort = SerialPort.getCommPort(portList.getValue().toString());
+            chosenPort.setComPortTimeouts(SerialPort.TIMEOUT_SCANNER, 0, 0);
+            if (chosenPort.openPort()) {
+                btn.setText("End");
+                portList.setEditable(false);
+                out = chosenPort.getOutputStream();
+            }
 
-                }
+        } else {
+            // disconnect from the serial port
+            chosenPort.closePort();
+            try {
+                out.close();
+            } catch (IOException ex) {
+                Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            portList.setEditable(true);
+            btn.setText("Start");
+
+        }
     }
 
     public void onKeyPressed(KeyEvent event) {
+        int sliderValue = (int) verticalSlider.getValue();
         // soliman's code
         key = event.getCode();
-                switch(key)
-                {
-                    case UP:
-                {
-                    try {
-                        out.write('f');
-                        
-                    } catch (IOException ex) {
-                        Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+        switch (key) {
+            case UP: 
+                try {
+                   out.write('f');
+
+              } catch (IOException ex) {
+                    Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+
+            
+            break;
+            case DOWN: 
+                try {
+                    out.write('b');
+  
+                } catch (IOException ex) {
+                    Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);                
+                }
+            
+            break;
+            case LEFT: 
+                try {
+                    out.write('l');
+
+                } catch (IOException ex) {
+                    Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            
+            break;
+            case RIGHT: 
+                try {
+                    out.write('r');
+
+                } catch (IOException ex) {
+                    Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            
+            break;
+            
+            case B:
+                  sliderValue = sliderValue - 5;
+                    if (sliderValue < 0) {
+                        sliderValue = 0;
                     }
-                    
-                }
-                        break;
-                    case DOWN:
-                {
-                    try {
-                        out.write('b');
-                        
-                    } catch (IOException ex) {
-                        Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+                    verticalSlider.setValue(sliderValue);
+                    spedometer.setValue((sliderValue * 50) / 255);
+ 
+            break;
+            case S:
+          
+                    sliderValue = sliderValue + 5;
+                    if (sliderValue > 255) {
+                        sliderValue = 255;
                     }
-                }
-                        break;
-                    case LEFT:
-                {
-                    try {
-                        out.write('l');
-                        
-                    } catch (IOException ex) {
-                        Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                }
-                        break;
-                    case RIGHT:
-                {
-                    try {
-                        out.write('r');
-                        
-                    } catch (IOException ex) {
-                        Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-                }
-                        break;
-                }
+                    verticalSlider.setValue(sliderValue);
+                    spedometer.setValue((sliderValue * 50) / 255);
+ 
+            break;
+            
+            
+        }
+
     }
+
+    @FXML
+    void dragMouseSlider(MouseEvent event) {
+
+        int sliderValue = (int) verticalSlider.getValue();
+
+        //value to be sent to the Spedo Meter  
+        float SpedoMeterValue = (float) (sliderValue * 50) / 255;
+        spedometer.setValue(SpedoMeterValue);
+
+        //value to be sent to the Ardiuno 
+        int ArdiunoSpeedValue = sliderValue * (-1);
+
+        // System.out.println(sliderValue + " ");
+        //label1.setText(verticalSlider.getValue()+" ");
+    }
+    
 
 }
